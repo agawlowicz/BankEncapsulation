41f48a17d7f033ea18581be0cae2f2f191673936
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-03-30 14:47:48 -0500

    deposit and get balance, practice with encapsulation

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+        private double _balance = 0;
+
+        public void Deposit(double depositValue)
+        {
+            _balance += depositValue;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userBankAccount = new BankAccount();
+
+            bool isValid;
+            double depositResult;
+
+            do
+            {
+                Console.WriteLine("How much money would you like to deposit?");
+                isValid = double.TryParse(Console.ReadLine(), out depositResult);
+            } while (!isValid);
+
+            userBankAccount.Deposit(depositResult);
+
+            Console.WriteLine($"\nYour current balance is ${userBankAccount.GetBalance()}");
+        }
+    }
+}
