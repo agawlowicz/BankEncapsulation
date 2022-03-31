2c443fb71a00e1ada5a62386c81858ea43cf0cf9
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-03-31 09:52:36 -0500

    modified method to shorten main program

--- BankEncapsulation/BankAccount.cs
+++ BankEncapsulation/BankAccount.cs
@@ -8,8 +8,16 @@ namespace BankEncapsulation
         }
         private double _balance = 0;
 
-        public void Deposit(double depositValue)
+        public void Deposit()
         {
+            bool isValid;
+            double depositValue;
+            do
+            {
+                Console.WriteLine("How much money would you like to deposit?");
+                isValid = double.TryParse(Console.ReadLine(), out depositValue);
+            } while (!isValid);
+
             _balance += depositValue;
         }
 
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -8,16 +8,7 @@ namespace BankEncapsulation
         {
             var userBankAccount = new BankAccount();
 
-            bool isValid;
-            double depositResult;
-
-            do
-            {
-                Console.WriteLine("How much money would you like to deposit?");
-                isValid = double.TryParse(Console.ReadLine(), out depositResult);
-            } while (!isValid);
-
-            userBankAccount.Deposit(depositResult);
+            userBankAccount.Deposit();
 
             Console.WriteLine($"\nYour current balance is ${userBankAccount.GetBalance()}");
         }
